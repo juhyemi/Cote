04c99b4cbebcc948c1a512ff73a20f86690cab69
LimEunSang <86942472+LimEunSang@users.noreply.github.com>
2024-07-15 22:47:20 +0900

    [Soved_은상] 폰켓몬

---
+++ 20240715/폰켓몬.cpp
@@ -0,0 +1,20 @@
+#include <vector>
+#include <map>
+#include <algorithm>
+using namespace std;
+
+int type;
+map<int, bool> m;
+
+int solution(vector<int> nums) {
+    for(const auto& num : nums) {
+        // 이미 존재하는 종류의 폰켓몬일 때
+        if(m[num]) continue;
+        
+        // 그렇지 않을 때
+        ++type;
+        m[num] = true;
+    }
+    
+    return min(type, (int)nums.size() / 2);
+}
