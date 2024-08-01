b5f8b9a36a9abf2e937c8fa668153e56a67d6323
KIMJUHYE <114149911+juhyemi@users.noreply.github.com>
2024-08-01 23:24:30 +0900

    [Solved_주혜] 가장 큰 수

---
+++ 20240801/BestNumber.java
@@ -0,0 +1,13 @@
+//가장 큰 수
+import java.util.*;
+class BestNumber {
+    public String bestNumber(int[] numbers) {
+        String answer = "";
+        String[] str = new String[numbers.length];
+        for(int i=0; i<numbers.length; i++) str[i] = String.valueOf(numbers[i]);
+        Arrays.sort(str, (o1,o2)->(o2+o1).compareTo(o1+o2));
+        if(str[0].equals("0")) return "0";
+        for(String s:str) answer+=s;
+        return answer;
+    }
+}
