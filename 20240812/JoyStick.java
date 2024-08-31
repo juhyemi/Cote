4db18612619a698f52edf68211d0bbf3fa5d8905
KIMJUHYE <114149911+juhyemi@users.noreply.github.com>
2024-08-31 15:29:12 +0900

    [Solved_주혜] 조이스틱

--- 20240812/JoyStick.java
+++ 20240812/JoyStick.java
@@ -13,8 +13,11 @@ public class JoyStick {
                 while(index < name.length() && name.charAt(index) == 'A'){
                     index++;
                 }
-                move = Math.min(move, i * 2 + name.length() - index);
-                move = Math.min(move, (name.length() - index) * 2 + i);
+            // 좌우 이동 횟수의 최소값을 계산
+            // 1. 현재까지 이동한 거리(i) + 뒤로 돌아서 처음으로 가는 거리(name.length() - index)
+            move = Math.min(move, i * 2 + name.length() - index);
+            // 2. 반대로 뒤에서 앞으로 가는 경우의 거리((name.length() - index) * 2 + i)
+            move = Math.min(move, (name.length() - index) * 2 + i);
             }
             return answer+move;
         }
